524e7f4290697524bea4fbe7b26995465f16acf8
Gina Therese Hvidsten <gthvidsten@whitestone.no>
2019-05-23 20:46:55 +0200

    Initial application

---
+++ src/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Mef2DiExtensions
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            IHostBuilder builder = new HostBuilder()
+                .ConfigureServices((hostBuilderContext, services) =>
+                {
+                    services.AddHostedService<Service>();
+                });
+
+            await builder.RunConsoleAsync();
+        }
+    }
+}
---
+++ src/Service.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace Mef2DiExtensions
+{
+    public class Service : IHostedService
+    {
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Service Starting");
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Service Stopping");
+        }
+    }
+}
